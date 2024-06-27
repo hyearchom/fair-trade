fc9cf8d916f85ed83e3d1972cfec4b49d318c67c
Sirmori <askmori@protonmail.com>
2024-06-27 22:47:57 +0200

    Pohyb lodi
    - první verze dokonalého pohybu
    - kamera sleduje loď s odstupem
    - ekliptické pozadí

--- hrac.gd
+++ hrac.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const CITLIVOST_MYSI = 0.01
+const RYCHLOST = 5.0
+const CITLIVOST_MYSI = 0.001
 const ZORNE_POLE = 90.0
 
-@onready var Oci = $Hlava/Oci
+var zamek_menevrovani
+
+@onready var Vertikala = $Horizontala/Vertikala
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -14,46 +15,46 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
-	#print($Hlava/Oci/Dotek.get_collider())
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("skok") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
-	#var input_dir = Input.get_vector("doleva", "doprava", "dopredu", "dozadu")
+	var input_dir = Input.get_vector("doleva", "doprava", "dopredu", "dozadu")
 	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var direction = ($Hlava/Oci.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = ($Horizontala/Vertikala.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if input_dir:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		#velocity.x = direction.x * RYCHLOST
+		#velocity.y = direction.y * RYCHLOST
+		#velocity.z = direction.z * RYCHLOST
+		velocity = direction * RYCHLOST
+		zamek_menevrovani = false
+		$pruzkumnik.transform.basis = Basis.looking_at(velocity)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		#velocity = move_toward(velocity, 0, Vector3.ONE *RYCHLOST)
+		velocity.x = move_toward(velocity.x, 0, RYCHLOST)
+		velocity.y = move_toward(velocity.y, 0, RYCHLOST)
+		velocity.z = move_toward(velocity.z, 0, RYCHLOST)
+		zamek_menevrovani = true
 
 	move_and_slide()
+	print(velocity)
 
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		zamirit_pohled(event)
-	#elif event is InputEventMouseButton:
-		#_zvolit_akci(event)
+	if not zamek_menevrovani:
+		if event is InputEventMouseMotion:
+			zamirit_pohled(event)
+		#elif event is InputEventMouseButton:
+			#_zvolit_akci(event)
 
 func zamirit_pohled(event):
 	var sirka = -event.relative.x *CITLIVOST_MYSI
 	var vyska = event.relative.y *CITLIVOST_MYSI
-		
-	$Hlava.rotate_y(sirka)
-	Oci.rotate_x(vyska)
-	Oci.rotation.x = clampf(Oci.rotation.x, -deg_to_rad(ZORNE_POLE), deg_to_rad(ZORNE_POLE))
+	var mez: float = 0.015
+	
+	$Horizontala.rotate_y(clampf(sirka, -mez, mez))
+	#$pruzkumnik.rotate_y(clampf(sirka, -mez, mez))
+	Vertikala.rotate_x(clampf(vyska, -mez, mez))
+	#$pruzkumnik.rotate_x(clampf(vyska, -mez, mez))
+	#Vertikala.rotation.x = clampf(Vertikala.rotation.x, -deg_to_rad(ZORNE_POLE), deg_to_rad(ZORNE_POLE))
 
 #func _zvolit_akci(event):
-	#var terc = $Hlava/Oci/Dotek.get_collider()
+	#var terc = $Horizontala/Vertikala/Dotek.get_collider()
 	#if terc:
 		#if event.is_action_pressed("retez"):
 			#$Akce.spoustat(terc)
