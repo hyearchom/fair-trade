567ce59cd8e28d00950558082cb604b4e483e11e
Sirmori <askmori@protonmail.com>
2024-07-26 17:34:44 +0200

    Dokončena scéna střely a vzhled soupeře
    - vzhled střely dokončen
    - navigace střely dokončena
    - přidána stopa za střelou
    - přidán prvotní model nepřítele

--- sceny/strela.gd
+++ sceny/strela.gd
@@ -2,23 +2,32 @@ extends CharacterBody3D
 
 const MAX_VYKON = 0.2
 
-var cil: Vector3
-
 func _ready() -> void:
 	add_to_group('souperi')
+	$Rozlet.start()
+
 
 func _physics_process(_delta: float) -> void:
-	cil = _najit_hrace()
-	var smer = (cil -global_position).normalized()
+	var smer = Vector3.BACK
+	
+	if $Rozlet.is_stopped():
+		var cil = _najit_hrace()
+		smer = _navadeni_za_hracem(cil)
+		look_at(cil)
 	
 	var srazka = move_and_collide(smer *MAX_VYKON)
-	look_at(cil)
 
 	if srazka:
-		queue_free()
+		#queue_free()
 		if srazka.get_collider().is_in_group('hrac'):
 			print('Zničen!')
+
 
 func _najit_hrace() -> Vector3:
 	var hrac := get_tree().get_first_node_in_group('hrac')
 	return hrac.global_position
+
+
+func _navadeni_za_hracem(cil: Vector3) -> Vector3:
+	var smer = (cil -global_position).normalized()
+	return smer
